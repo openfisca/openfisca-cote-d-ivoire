2edd2d4055f8db3cb1ed5b280f2195ba1cb8b8b5
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-09-20 18:21:46 +0200

    WIP 2

--- openfisca_cote_d_ivoire/__init__.py
+++ openfisca_cote_d_ivoire/__init__.py
@@ -4,7 +4,7 @@ import os
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
-from . import entities
+from openfisca_cote_d_ivoire import entities
 
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -22,4 +22,5 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
 
         # We add to our tax and benefit system all the legislation parameters defined in the  parameters files
         param_path = os.path.join(COUNTRY_DIR, 'parameters')
+        print(param_path)
         self.load_parameters(param_path)
