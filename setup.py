48b792d439e1d9667884aa892e412ac5a35cf3d7
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-09-20 22:37:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     version='0.4.0',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
-    description=u'OpenFisca tax and benefit system for CO   TE_D_IVOIRE',
+    description=u'OpenFisca tax and benefit system for COTE_D_IVOIRE',
     keywords='benefit microsimulation social tax',
     license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
     url='https://github.com/openfisca/country-template',
