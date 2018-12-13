7ab7f639b7c120ac8c44cc569a28fe952fd8ca15
Léo Czajka 2 <leo.czajka@gmail.com>
2018-12-13 16:46:17 +0100

    Remove print

--- openfisca_cote_d_ivoire/__init__.py
+++ openfisca_cote_d_ivoire/__init__.py
@@ -23,5 +23,4 @@ class CountryTaxBenefitSystem(TaxBenefitSystem):
 
         # We add to our tax and benefit system all the legislation parameters defined in the  parameters files
         param_path = os.path.join(COUNTRY_DIR, 'parameters')
-        print(param_path)
         self.load_parameters(param_path)
