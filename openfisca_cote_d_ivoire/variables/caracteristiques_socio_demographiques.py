5961629b81d516cab7677aebfc86336d273a8955
Léo Czajka 2 <leo.czajka@gmail.com>
2019-01-16 17:42:22 +0100

    adds variable definition in caracteristiques_socio + cosmetic changes to entities

--- openfisca_cote_d_ivoire/variables/caracteristiques_socio_demographiques.py
+++ openfisca_cote_d_ivoire/variables/caracteristiques_socio_demographiques.py
@@ -11,9 +11,63 @@ class age(Variable):
     label = u"Âge de l'individu (en années)"
 
 
-class nombre_de_parts(Variable):
-    value_type = int
-    default_value = 1
+class occupation_principale_taxee(Variable):
+    value_type = bool
     entity = Person
     definition_period = YEAR
-    label = "Nombre de parts fiscales"
+    label = u"Variable binaire déterminant si les revenus de l'activité principale sont taxés ou non "
+
+
+class proprietaire_resid_principale(Variable):
+    value_type = bool
+    entity = Household
+    definition_period = YEAR
+    label = u"Variable binaire déterminant si l'habitation est détenue par le ménage"
+
+class residence_principale_taxee(Variable):
+    value_type = bool
+    entity = Household
+    definition_period = YEAR
+    label = u"Variable binaire déterminant si le menage paie des impôts sur sa résidence principale"
+
+
+class nb_persons(Variable):
+    value_type = int
+    entity = Household
+    definition_period = YEAR
+    label = "Nombre de personnes dans le ménage"
+
+    def formula(household, period):
+        nb_persons = household.nb_persons()
+        return nb_persons
+
+class nb_enfants_a_charge(Variable):
+    value_type = int
+    entity = Household
+    definition_period = YEAR
+    label = "Nombre d'enfants de la personne de référence et de son/sa conjointe dans le ménage"
+
+    def formula(household, period):
+        nb_enfants_a_charge = household.nb_persons(Household.ENFANT)
+        return nb_enfants_a_charge
+
+class nb_conjoint(Variable):
+    value_type = int
+    entity = Household
+    definition_period = YEAR
+    label = "Variable binaire indiquant si la personne de référence a un-e conjoint-e dans le ménage"
+
+    def formula(household, period):
+        nb_conjoint = household.nb_persons(Household.CONJOINT)
+        return nb_conjoint
+
+class nombre_de_parts(Variable):
+    value_type = int
+    entity = Person
+    definition_period = YEAR
+    label = "Nombre de parts fiscales pour le calcul de l'impôt sur le revenu"
+
+    def formula(household, period):
+        # reprendre ici
+        return nombre_de_parts
+
