e71c2c59a6b99126a02869ea3905d4e322361c0b
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-02-15 14:07:36 +0100

    Rename salaire_brut_formel to salaire

--- openfisca_cote_d_ivoire/variables/revenus.py
+++ openfisca_cote_d_ivoire/variables/revenus.py
@@ -5,7 +5,7 @@ from openfisca_core.model_api import *
 from openfisca_cote_d_ivoire.entities import *
 
 
-class salaire_brut_formel(Variable):
+class salaire(Variable):
     value_type = float
     entity = Person
     definition_period = YEAR
