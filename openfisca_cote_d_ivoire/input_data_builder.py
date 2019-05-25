46c601777475b544d4026dc5fe874c18cf1aa137
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-05-25 15:12:26 +0200

    Explore-aggregates

--- openfisca_cote_d_ivoire/input_data_builder.py
+++ openfisca_cote_d_ivoire/input_data_builder.py
@@ -28,9 +28,9 @@ def get_data_file_path():
 
 def create_dataframes_from_stata_data():
     data_file_path = get_data_file_path()
-    # import pprint
-    # dico_labels = pd.read_stata(data_file_path, iterator=True)
-    # pprint.pprint(dico_labels.variable_labels())
+    import pprint
+    dico_labels = pd.read_stata(data_file_path, iterator=True)
+    pprint.pprint(dico_labels.variable_labels())
     dataframe = pd.read_stata(data_file_path)
     person_variables = [
         'age',
@@ -38,6 +38,7 @@ def create_dataframes_from_stata_data():
         'hhid',
         'id',
         'inc_act1_ind',
+        'inc_pension_ind',
         'link_to_head',
         'pond',
         'sex',
@@ -45,8 +46,10 @@ def create_dataframes_from_stata_data():
         ]
     person_dataframe = dataframe[person_variables].copy()
     person_dataframe['salaire'] = person_dataframe.inc_act1_ind * (
-        (person_dataframe.formel_informel == 1) | (person_dataframe.formel_informel == 1)
+        (person_dataframe.formel_informel == 1) | (person_dataframe.formel_informel == 2)
         )
+    print(person_dataframe.formel_informel.value_counts(dropna = False))
+    person_dataframe['pension'] = person_dataframe.inc_pension_ind
     person_dataframe['household_role_index'] = (
         0 * (person_dataframe.link_to_head == 'chef de menage')
         + 1 * (person_dataframe.link_to_head == 'epouse ou mari')
