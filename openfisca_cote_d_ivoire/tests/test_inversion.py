97c60ec822fe97e14b10ce55b57f7004d22e4570
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-02-26 22:37:36 +0100

    WIP

---
+++ openfisca_cote_d_ivoire/tests/test_inversion.py
@@ -0,0 +1,29 @@
+import logging
+
+
+from openfisca_core.simulation_builder import SimulationBuilder
+
+from openfisca_cote_d_ivoire import CountryTaxBenefitSystem as CoteDIvoireTaxBenefitSystem
+
+
+log = logging.getLogger(__name__)
+
+
+tax_benefit_system = CoteDIvoireTaxBenefitSystem()
+# prelevements_sociaux = tax_benefit_system.parameters.prelevements_obligatoires.prelevements_sociaux
+
+# famille = prelevements_sociaux.
+
+year = 2013
+test_case = {
+    'persons': {
+        'A': {
+            'salaire': {year: 100000}
+            },
+        },
+    }
+
+simulation_builder = SimulationBuilder()
+simulation = simulation_builder.build_from_entities(tax_benefit_system, test_case)
+
+print(simulation.calculate('famille', period = year))
--- openfisca_cote_d_ivoire/variables/prelevements_obligatoires/prelevements_sociaux.py
+++ openfisca_cote_d_ivoire/variables/prelevements_obligatoires/prelevements_sociaux.py
@@ -1,2 +1,51 @@
-# from openfisca_core.model_api import *
-# from openfisca_cote_d_ivoire.entities import *
+from openfisca_core.model_api import *
+from openfisca_cote_d_ivoire.entities import *
+
+
+class accidents_du_travail(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Cotisation sociale accidents du travail (employeur)"
+
+    def formula(person, period, parameters):
+        salaire = person('salaire', period)
+        accidents_du_travail = parameters(period).prelevements_obligatoires.prelevements_sociaux.accidents_du_travail
+        taux_minimal = accidents_du_travail.taux_minimal
+        return taux_minimal * salaire
+
+
+class famille(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Cotisation sociale prestations familiales (employeur)"
+
+    def formula(person, period, parameters):
+        salaire = person('salaire', period)
+        famille = parameters(period).prelevements_obligatoires.prelevements_sociaux.prestations_familiales
+        return famille.calc(salaire)
+
+
+class retraite_employeur(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Cotisation sociale retraite (employeur)"
+
+    def formula(person, period, parameters):
+        salaire = person('salaire', period)
+        retraite = parameters(period).prelevements_obligatoires.prelevements_sociaux.retraite
+        return retraite.employeur.calc(salaire)
+
+
+class retraite_salarie(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    label = "Cotisation sociale retraite (salarié)"
+
+    def formula(person, period, parameters):
+        salaire = person('salaire', period)
+        retraite = parameters(period).prelevements_obligatoires.prelevements_sociaux.retraite
+        return retraite.salarie.calc(salaire)
