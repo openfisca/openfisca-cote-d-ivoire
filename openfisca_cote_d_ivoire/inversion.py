dd3005f4d44f12d50c9751ce55321c7f517d9b20
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-03-04 11:44:54 +0100

    Add nombre de part for inversion

--- openfisca_cote_d_ivoire/inversion.py
+++ openfisca_cote_d_ivoire/inversion.py
@@ -9,13 +9,15 @@ class salaire_brut(Variable):
     label = "Salaire brut"
 
     def formula(person, period, parameters):
+        salaire = person('salaire', period)
+        nombre_de_parts = person.household('nombre_de_parts', period)
+
         impot_revenu = parameters(period).prelevements_obligatoires.impot_revenu.bareme.copy()
         abattement = parameters(period).prelevements_obligatoires.impot_revenu.abattement
         # We have to deal with abattement
         impot_revenu.multiply_thresholds(1.0 / abattement)
         impot_revenu.multiply_rates(abattement)
         prelevements_sociaux = parameters(period).prelevements_obligatoires.prelevements_sociaux.retraite.salarie
-        salaire = person('salaire', period)
-        salaire_imposable = impot_revenu.inverse().calc(salaire)
+        salaire_imposable = nombre_de_parts * impot_revenu.inverse().calc(salaire / nombre_de_parts)
         salaire_brut = 12 * prelevements_sociaux.inverse().calc(salaire_imposable / 12)
         return salaire_brut
